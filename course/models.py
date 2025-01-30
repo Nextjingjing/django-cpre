37b91088db0a520bd317d4b0810dc33944e9b246
SNeXT <snext2004@gmail.com>
2025-01-31 02:47:50 +0700

    curriculum mapping

--- course/models.py
+++ course/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from curriculum.models import Curriculum
+
 
 # Create your models here.
 class Course(models.Model):
@@ -13,3 +15,23 @@ class Course(models.Model):
 
     def __str__(self):
         return f"{self.id} - {self.name}"
+    
+class CurriculumMapping(models.Model):  
+    curriculum = models.ForeignKey(
+        Curriculum, 
+        on_delete=models.CASCADE, 
+        related_name="curriculum_mappings"
+    )  
+    course = models.ForeignKey(
+        Course, 
+        on_delete=models.CASCADE, 
+        related_name="curriculum_mappings"
+    )  
+
+    class Meta:
+        verbose_name = "Curriculum Mapping"
+        verbose_name_plural = "Curriculum Mappings"
+        unique_together = ('curriculum', 'course')  # ป้องกันการเพิ่มซ้ำ
+
+    def __str__(self):
+        return f"{self.curriculum.name} - {self.course.name}"
