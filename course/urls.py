5f4bc41225b1b9eaba4c62f05e15edac109f7be3
SNeXT <snext2004@gmail.com>
2025-01-31 01:18:42 +0700

    add search course view

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CourseList, CourseDetail
+from .views import CourseList, CourseDetail, CourseSearch
 
 urlpatterns = [
     path('list/', CourseList.as_view(), name='course-list'),
     path('list/<id>/', CourseDetail.as_view(), name='course-detail'),
+    path('search/<str:search>/', CourseSearch.as_view(), name='course-search'),
 ]
--- course/views.py
+++ course/views.py
@@ -14,4 +14,13 @@ class CourseDetail(generics.RetrieveAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     lookup_field = 'id'
-    
+
+class CourseSearch(generics.ListAPIView):
+    serializer_class = CourseSerializer
+
+    def get_queryset(self):
+        queryset = Course.objects.all()
+        search_query = self.kwargs.get('search', None)
+        if search_query:
+            queryset = queryset.filter(name__startswith=search_query)
+        return queryset
