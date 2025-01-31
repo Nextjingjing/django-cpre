62f4e39d580ec0728ad1587e64bf48b27931b8cc
SNeXT <snext2004@gmail.com>
2025-01-31 09:15:15 +0700

    add curriculum lates view

--- curriculum/urls.py
+++ curriculum/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CurriculumList, CurriculumDetail
+from .views import CurriculumList, CurriculumDetail, CurriculumLatestView
 
 urlpatterns = [
     path('list/', CurriculumList.as_view(), name='curriculumList-list'),
     path('list/<id>/', CurriculumDetail.as_view(), name='curriculum-detail'),
+    path('latest/', CurriculumLatestView.as_view(), name='curriculum-latest'),
 ]
--- curriculum/views.py
+++ curriculum/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .serializers import CurriculumSerializer
-from .models import Curriculum
+from .models import Curriculum , CurriculumMapping
 from rest_framework import generics
+from rest_framework.views import APIView
+from django.http import JsonResponse
+
 
 # Create your views here.
 class CurriculumList(generics.ListAPIView):
@@ -12,3 +15,17 @@ class CurriculumDetail(generics.RetrieveAPIView):
     queryset = Curriculum.objects.all()
     serializer_class = CurriculumSerializer
     lookup_field = 'id'
+
+class CurriculumLatestView(APIView):
+    """ 🔹 Get the latest curriculum and its courses """
+    def get(self, request):
+        last_curriculum = Curriculum.objects.order_by("-id").first()
+        if not last_curriculum:
+            return JsonResponse({"error": "No curriculum found"}, status=404)
+
+        courses = last_curriculum.curriculum_mappings.all()
+        data = [
+            {"id": cm.course.id, "name": cm.course.name, "year": cm.course.year, "term": cm.course.term}
+            for cm in courses
+        ]
+        return JsonResponse({"curriculum": last_curriculum.name, "courses": data}, safe=False)
