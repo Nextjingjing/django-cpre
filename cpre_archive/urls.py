2ebe3725126068342097f5a4d8d47234c847cf2a
SNeXT <snext2004@gmail.com>
2025-01-31 09:26:17 +0700

    fix endpoints

--- cpre_archive/urls.py
+++ cpre_archive/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/course/', include('course.urls')),
-    path('api/curriculum/', include('curriculum.urls')),
+    path('api/courses/', include('course.urls')),
+    path('api/curriculums/', include('curriculum.urls')),
 ]
--- curriculum/urls.py
+++ curriculum/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('list/', CurriculumList.as_view(), name='curriculumList-list'),
     path('list/<id>/', CurriculumDetail.as_view(), name='curriculum-detail'),
     path('latest/', CurriculumLatestView.as_view(), name='curriculum-latest'),
-    path("<str:curriculum_id>/course/", CurriculumCoursesView.as_view(), name="curriculum-courses"),
+    path("<str:curriculum_id>/courses/", CurriculumCoursesView.as_view(), name="curriculum-courses"),
 ]
